ce5add72227f2711e2624b39970e2e2741d536c6
灵感屋 <admin@lgwu.net>
2026-05-01 21:50:22 +0800

    Update diy-part5-6.6.sh

--- diy-part5-6.6.sh
+++ diy-part5-6.6.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 #
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part5-6.6.sh
-# Description: OpenWrt DIY script part 5 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
+# DIY Part 5 for 6.6 branch - After feeds install
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+# 修改默认 IP（可选）
+# sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# 修改主机名（可选）
+# sed -i 's/OpenWrt/AX6000-6.6/g' package/base-files/files/bin/config_generate
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# 确保 Lucky 和 HomeProxy 被正确安装
+if [ -d "package/luci-app-lucky" ]; then
+    echo "Lucky 已添加"
+fi
+if [ -d "package/homeproxy" ]; then
+    echo "HomeProxy 已添加"
+fi
+
+echo "diy-part5-6.6.sh 执行完成"
