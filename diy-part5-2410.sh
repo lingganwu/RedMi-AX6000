6df4e2adde6c1a337faf7d3b33ace0e1728ae0b6
灵感屋 <admin@lgwu.net>
2026-05-01 21:50:04 +0800

    Update diy-part5-2410.sh

--- diy-part5-2410.sh
+++ diy-part5-2410.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 #
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part5-2410.sh
-# Description: OpenWrt DIY script part 5 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
+# DIY Part 5 for 2410 branch - After feeds install
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+# 修改默认 IP（可选）
+# sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# 修改主机名（可选）
+# sed -i 's/OpenWrt/AX6000-2410/g' package/base-files/files/bin/config_generate
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# 确保 Lucky 和 HomeProxy 被正确安装
+if [ -d "package/luci-app-lucky" ]; then
+    echo "Lucky 已添加"
+fi
+if [ -d "package/homeproxy" ]; then
+    echo "HomeProxy 已添加"
+fi
 
-# 移除USB网络共享
-sed -i 's/kmod-usb-net-rndis //g' target/linux/mediatek/image/mt7986.mk
+echo "diy-part5-2410.sh 执行完成"
